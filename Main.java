d1aa50cefa4e70cdd36e2d2ada53519b768bed7f
Rylen27 <robertroden@rocketmail.com>
2020-04-28 16:02:48 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -37,7 +37,7 @@ public class Main {
 	{
 		ModRecipes.init();
 		OreDictionaryInit.registerOres();
-		RegistryHandler.initRegistries();
+		RegistryHandler.initRegistries(event);
 	}
 	
 	@EventHandler
